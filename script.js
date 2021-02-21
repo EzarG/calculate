026480c61143603a996d01b231db8ac81c2af838
IhorK <Igor.kalganow75@gmail.com>
2021-02-21 20:11:52 +0200

    update calculate

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
 const display = document.querySelector('.display');
-
-document.querySelectorAll('.digits button')
-    .forEach(button => button.addEventListener('click', digitPressed));
-function  digitPressed(even) {
-   display.value += even.target.innerText;
-}
-document.querySelectorAll('.opers button')
-    .forEach(button => button.addEventListener('click', opersPressed));
-function  opersPressed(even) {
+document.querySelectorAll('.item')
+    .forEach(item => item.addEventListener('click', digitPressed));
+function digitPressed(even) {
     display.value += even.target.innerText;
-
 }
+
 document.querySelector('.eq').addEventListener('click', eqPressed);
 function eqPressed() {
     if (display.value.endsWith("/0")) {
+        document.getElementsByClassName('display').value = "";
         alert("cannot be divided by zero");
+        display.value = ' '
+
     }else {
         display.value = eval(display.value);
+
     }
 }
-document.querySelector('.clear').addEventListener('click', clearAll);
-
-function clearAll() {
+document.querySelector('.item-clear').addEventListener('click', clearAll);
+function clearAll(){
     display.value = ' '
 }
-document.querySelector('back').addEventListener('click', backOneStep);
 
-function backOneStep() {
+document.querySelector('.item-back').addEventListener('click',backOne)
+function backOne() {
     display.value = display.value.slice(0, -1);
 }
-
-
-
-
-
-
-
-
 
